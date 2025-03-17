dd267102d11139ef6e1365e3ff8346955b82a05b
Nicolas K <nicolaskay95200@gmail.com>
2025-03-17 13:47:05 +0100

    Ajout du chemin du fichier PDF dans les fixtures pour l'entité Book

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -29,47 +29,14 @@ class AppFixtures extends Fixture
             $tags[] = $tag;
         }
 
+        $pdfFilePath = '/uploads/pdf/Lorem.pdf';
+
         for ($i = 0; $i < 150; $i++) {
             $book = new Book();
-
-            $openlib = $faker->numberBetween(1000000, 9000000);
-
-            try {
-                $bookData = json_decode(file_get_contents("https://openlibrary.org/api/books?bibkeys=OLID:{$openlib}&format=json&jscmd=data"), true);
-
-                if ($bookData && isset($bookData["OLID:{$openlib}"])) {
-                    $data = $bookData["OLID:{$openlib}"];
-
-                    $book->setName($data['title'] ?? $faker->sentence(1, 4));
-
-                    if (isset($data['authors']) && !empty($data['authors'])) {
-                        $book->setAuthor($data['authors'][0]['name']);
-                    } else {
-                        $book->setAuthor($faker->name);
-                    }
-
-                    if (isset($data['description'])) {
-                        $description = is_array($data['description']) ? ($data['description']['value'] ?? '') : $data['description'];
-                        $book->setDescription($description ?: $faker->paragraph(3));
-                    } else {
-                        $book->setDescription($faker->paragraph(3));
-                    }
-
-                    if (isset($data['cover']) && isset($data['cover']['medium'])) {
-                        $book->setCover($data['cover']['medium']);
-                    } else {
-                        $book->setCover("https://covers.openlibrary.org/b/id/{$openlib}-M.jpg");
-                    }
-                } else {
-                    throw new \Exception("Données non disponibles");
-                }
-            } catch (\Exception $e) {
-                $book->setName($faker->sentence(1, 4));
-                $book->setAuthor($faker->name);
-                $book->setDescription($faker->paragraph(3));
-                $book->setCover("https://picsum.photos/seed/book{$i}/200/300");
-            }
-
+            $book->setName($faker->sentence(1,4));
+            $book->setAuthor($faker->name);
+            $book->setDescription($faker->paragraph(3));
+            $book->setCover("https://covers.openlibrary.org/b/id/" . $faker->numberBetween(1000000, 9000000) . "-M.jpg");
             $book->setPopularity($faker->numberBetween(0, 100));
             $book->setSlug($faker->slug);
             $book->setIsRestricted($faker->boolean(20));
@@ -77,6 +44,7 @@ class AppFixtures extends Fixture
             $createdAtString = $book->getCreatedAt()->format('Y-m-d H:i:s');
             $book->setUpdatedAt(\DateTimeImmutable::createFromMutable($faker->dateTimeBetween($createdAtString, 'now')));
             $book->setIsReserved($faker->boolean(30));
+            $book->setPdfFile($pdfFilePath);
 
             $bookTags = $faker->randomElements($tags, $faker->numberBetween(1, 3));
             foreach ($bookTags as $tag) {
@@ -85,6 +53,7 @@ class AppFixtures extends Fixture
 
             $manager->persist($book);
         }
+
 
         $manager->flush();
     }
