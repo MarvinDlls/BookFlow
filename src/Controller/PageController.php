329ef966c9412fef9d447ea7ef066e8f9100f46a
Nicolas K <nicolaskay95200@gmail.com>
2025-03-17 19:29:01 +0100

    Supprimer la dépendance à GoogleApiService dans PageController et simplifier la méthode index

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Service\GoogleApiService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,13 +9,11 @@ use Symfony\Component\Routing\Attribute\Route;
 final class PageController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function index(GoogleApiService $googleApiService): Response
+    public function index(): Response
     {
-        $popularBooks = $googleApiService->fetchPopularBooks(5);
 
         return $this->render('page/index.html.twig', [
             'controller_name' => 'PageController',
-            'popularBooks' => $popularBooks
         ]);
     }
 }
