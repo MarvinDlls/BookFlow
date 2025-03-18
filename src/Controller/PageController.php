0269f6be44e652e2e7298643a9e824c2189ab009
Marvindlls <marvindillenseger@gmail.com>
2025-03-18 11:56:00 +0100

    Remove unused index method from PageController

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -8,12 +8,5 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class PageController extends AbstractController
 {
-    #[Route('/', name: 'app_homepage')]
-    public function index(): Response
-    {
-
-        return $this->render('page/index.html.twig', [
-            'controller_name' => 'PageController',
-        ]);
-    }
+    
 }
