49380cc71fb67367dfa59d3bd66ff3c478096a13
Nicolas K <nicolaskay95200@gmail.com>
2025-03-19 03:46:26 +0100

    Ajout de la documentation des méthodes dans le contrôleur de sécurité

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,8 +7,17 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
+/**
+ * Controller utilisé pour la gestion de la sécurité.
+ */
 class SecurityController extends AbstractController
 {
+    /**
+     * Permet de se connecter à l'application.
+     *
+     * @param AuthenticationUtils $authenticationUtils
+     * @return Response
+     */
     #[Route(path: '/login', name: 'app_login', methods: ['GET', 'POST'])]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
@@ -26,6 +35,9 @@ class SecurityController extends AbstractController
         ]);
     }
 
+    /**
+     * Permet de se déconnecter de l'application.
+     */
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
