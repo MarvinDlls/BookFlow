b7ac4422d62ae2b4c19ef71666a853a02551a41b
Nicolas K <nicolaskay95200@gmail.com>
2025-03-19 03:47:25 +0100

    Ajout de la documentation des méthodes dans BookService pour améliorer la clarté et la maintenabilité du code

--- src/Service/BookService.php
+++ src/Service/BookService.php
@@ -15,12 +15,16 @@ class BookService
     private PaginatorInterface $paginator;
     private EntityManagerInterface $entityManager;
 
-    public function __construct(
-        BookRepository $bookRepository,
-        TagRepository $tagRepository,
-        PaginatorInterface $paginator,
-        EntityManagerInterface $entityManager
-    ) {
+    /**
+     * Injecte les dépendances nécessaires pour la gestion des livres.
+     *
+     * @param BookRepository $bookRepository
+     * @param TagRepository $tagRepository
+     * @param PaginatorInterface $paginator
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(BookRepository $bookRepository, TagRepository $tagRepository, PaginatorInterface $paginator, EntityManagerInterface $entityManager)
+    {
         $this->bookRepository = $bookRepository;
         $this->tagRepository = $tagRepository;
         $this->paginator = $paginator;
@@ -29,8 +33,14 @@ class BookService
 
     /**
      * Récupère tous les livres.
+     *
+     * @param int $page
+     * @param int $limit
+     * @param int $tagId
+     * @param bool $sortByPopularity
+     * @return object
      */
-    public function fetchAllBooks(int $page, int $limit, int $tagId, bool $sortByPopularity)
+    public function fetchAllBooks(int $page, int $limit, int $tagId, bool $sortByPopularity): object
     {
         $queryBuilder = $this->entityManager->getRepository(Book::class)->createQueryBuilder('b');
 
@@ -53,6 +63,11 @@ class BookService
 
     /**
      * Recherche des livres par mot-clé.
+     *
+     * @param string $query
+     * @param int $maxResults
+     * @param int|null $startIndex
+     * @return array
      */
     public function searchBooks(string $query, int $maxResults = 10, ?int $startIndex = 0): array
     {
@@ -61,6 +76,9 @@ class BookService
 
     /**
      * Récupère les détails d'un livre par son ID.
+     *
+     * @param int $id
+     * @return Book|null
      */
     public function fetchBookById(int $id): ?Book
     {
@@ -73,6 +91,9 @@ class BookService
 
     /**
      * Récupère les livres les plus populaires.
+     *
+     * @param int $maxResults
+     * @return array
      */
     public function fetchPopularBooks(int $maxResults = 5): array
     {
@@ -82,6 +103,9 @@ class BookService
     
     /**
      * Ajoute un livre à la base de données.
+     *
+     * @param array $data
+     * @return Book
      */
     public function addBook(array $data): Book
     {
@@ -104,6 +128,8 @@ class BookService
 
     /**
      * Sélectionne un Tag depuis la base de données.
+     *
+     * @return array
      */
     public function getAllTags(): array
     {
